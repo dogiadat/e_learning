27a7a6618ea8046da61664f4b139bb145b420c55
mangusidan <manhnv130100@gmail.com>
2022-09-08 18:48:59 +0700

    init model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
-  root 'static_pages#home'
-  get 'static_pages/home'
+  get "sessions/new"
+  get "users/new"
+  root "static_pages#home"
+  get "static_pages/home"
 end
